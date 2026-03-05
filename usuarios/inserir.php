9f5d0ecad4696561915c5109bd4bdbf3dce41dc7
norasantoss124 <elbasnick4@gmail.com>
2026-03-05 16:14:27 -0400

    update 76
    123456

--- usuarios/inserir.php
+++ usuarios/inserir.php
@@ -1,6 +1,25 @@
 <?php
 require_once __DIR__ . '/../config.php';
 
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $nome = ($_POST['nome']);
+    $email = ($_POST['email']);
+    $senha = ($_POST['senha']);
+
+    echo $nome, $senha, $email;
+    
+    if($nome && $email && $senha) {
+        $sql = "INSERT INTO 
+        usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
+        $stmt = $pdo-> prepare ($sql);
+        $stmt ->execute([
+            ':nome'=> $nome,
+            ':email'=> $email,
+            ':senha'=> password_hash($senha,PASSWORD_DEFAULT)
+
+        ]);
+    }
+    }
 
 $titulo = "Adicionar Usuario |";
 require_once BASE_PATH . '/includes/cabecalho.php';
